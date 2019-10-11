aaef5c2d2661255e5e6501bdd9b694bdbf1d3cd2
Zhain HELM <zhelm@c5r5s4.wethinkcode.co.za>
2019-10-11 14:10:36 +0200

    OK

--- PHP Bootcamp/D02/ex01/one_more_time.php
+++ PHP Bootcamp/D02/ex01/one_more_time.php
@@ -1,40 +1,26 @@
 #!/usr/bin/php
 <?php
-//   '2014-06-25 14:50:03'
-// Mardi 12 Novembre 2013 12:02:21
 $array = preg_split("/\s+/", $argv[1]);
+$days = array("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
+$months = array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septebre", "octobre", "novembre", "decembre");
 if(count($array) != 5)
 	exit("Wrong format");
-if(strtolower($array[2]) == "janvier")
-	$month = "01";
-else if(strtolower($array[2]) == "fevrier")
-	$month = "02";
-else if(strtolower($array[2]) == "mars")
-	$month = "03";
-else if(strtolower($array[2]) == "avril")
-	$month = "04";
-else if(strtolower($array[2]) == "mai")
-	$month = "05";
-else if(strtolower($array[2]) == "juin")
-	$month = "06";
-else if(strtolower($array[2]) == "juillet")
-	$month = "07";
-else if(strtolower($array[2]) == "aout")
-	$month = "08";
-else if(strtolower($array[2]) == "septebre")
-	$month = "09";
-else if(strtolower($array[2]) == "octobre")
-	$month = "10";
-else if(strtolower($array[2]) == "novembre")
-	$month = "11";
-else if(strtolower($array[2]) == "decembre")
-	$month = "12";
+if(!array_search(strtolower($array[0]), $days))
+	exit("Day first please!!");
+if(($month = array_search(strtolower($array[2]), $months)))
+	$month++;
 else
-	exit("Wrong format");
+	exit ("Wrong format of month.... lern hw tu spell fr pls");
+if($array[1] < 1 || $array[1] > 31)
+	exit ("Do you not know how dates work??");
+if(strlen((string)$array[3]) != 4)
+	exit ("Are you from the future/past?? Get it right next time");
 $arraytmp = array($array[3], $month, $array[1]);
 $arr = implode("-", $arraytmp);
 $arraytmp = array($arr, $array[4]);
 $arr = implode(" ", $arraytmp);
-$time = strtotime($arr);
-echo "$time";
+date_default_timezone_set("Europe/Paris");
+if(($time = strtotime($arr)))
+	exit("$time\n");
+exit("wrong farmat");
 ?>
